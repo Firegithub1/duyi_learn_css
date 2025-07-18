02904dd7e0304b95e851ac0b972a76de1e173929
YiFireeriFiY <15811912009@163.com>
2025-07-18 15:11:10 +0800

    feat: 调试图片处理

--- 图片上下视差滚动/index.js
+++ 图片上下视差滚动/index.js
@@ -1,59 +1,60 @@
-const imgs = ["./img/1.png", "./img/2.png", "./img/3.jpg", "./img/4.jpg"];
-
-const container = document.querySelector(".scroll-container");
-let curIndex = 0;
-
-const getPrevIndex = () => {
-  return curIndex === 0 ? imgs.length - 1 : curIndex - 1;
-};
-const getNextIndex = () => {
-  return curIndex === imgs.length - 1 ? 0 : curIndex + 1;
-};
-
-const creatDiv = (i) => {
-  const div = document.createElement("div");
-  div.className = "item";
-  const img = document.createElement("img");
-  img.src = imgs[i];
-  div.appendChild(img);
-  container.appendChild(div);
-  // 还要加样式，所以不要忘记了return
-  return div;
-};
-
-function getContent() {
-  // 会反复调用，所以清空一下
-  container.innerHTML = "";
-  const prevIndex = getPrevIndex();
-  const nextIndex = getNextIndex();
-  const prevDiv = creatDiv(prevIndex);
-  creatDiv(curIndex);
-  const nextDiv = creatDiv(nextIndex);
-  prevDiv.classList.add("prev");
-  nextDiv.classList.add("next");
+const imgs = ["./img/1.png", "./img/2.png", "./img/3.jpg", "./img/4.jpg"]
+// 1. 获取容器
+const container = document.querySelector(".scroll-container")
+let curIndex = 0
+// 1.2 更换图片（滚动到顶/底，移动到底/顶）
+function getPreIndex () {
+  return curIndex - 1 < 0 ? imgs.length - 1 : curIndex - 1
+}
+function getNextIndex () {
+  return curIndex + 1 >= imgs.length ? 0 : curIndex + 1
 }
 
-// 避免一直重复触发，导致跳跃
-let animationDoing = false;
-window.addEventListener("wheel", (e) => {
-  if (!e.deltaY || animationDoing) {
-    return;
-  }
-  animationDoing = true;
+// 2. 创建图片
+function createDiv (index) {
+  let div = document.createElement("div")
+  div.className = 'item'
+  let img = document.createElement("img")
+  img.src = imgs[index]
+  div.appendChild(img)
+  container.appendChild(div)
+  return div
+}
+
+// 2.1 添加样式
+
+// 3. 初始配置
+function getContent () {
+  container.innerHTML = ''
+  let prevIndex = getPreIndex()
+  let nextIndex = getNextIndex()
+  let prevDiv = createDiv(prevIndex)
+  createDiv(curIndex)
+  let nextDiv = createDiv(nextIndex)
+  prevDiv.classList.add('prev')
+  nextDiv.classList.add('next')
+}
+
+// 4.监听全局滚动事件
+let isTransition = false
+window.addEventListener('wheel', function (e) {
+  if (!e.deltaY || isTransition) return
+  isTransition = true
   if (e.deltaY > 0) {
-    curIndex = getNextIndex();
-    container.classList.add("scroll-down");
-  } else {
-    curIndex = getPrevIndex();
-    container.classList.add("scroll-up");
+    curIndex = getNextIndex()
+    container.classList.add('scroll-down')
   }
-});
+  if (e.deltaY < 0) {
+    curIndex = getPreIndex()
+    container.classList.add('scroll-up')
+  }
+})
 
-container.addEventListener("transitionend", (e) => {
-  container.classList.remove("scroll-down");
-  container.classList.remove("scroll-up");
-  animationDoing = false;
-  getContent();
-});
+// 6. 监听容器滚动事件，事件完成后在调用
+container.addEventListener('transitionend', function () {
+  container.classList.remove('scroll-up', 'scroll-down')
+  isTransition = false
+  getContent()
+})
 
-getContent();
+getContent()
